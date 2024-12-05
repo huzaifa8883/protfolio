bd2c63396c9442875f138bcdac586194daa84017
huzaifa56jbh <hadrish153@gmail.com>
2024-12-05 06:06:59 +0500

    fdf

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
 import './index.css';
 import App from './App.jsx';
-import Home from './components/Home.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
