b4a4fffdf86ad2e62bd9b242838b52ca1c0a19da
huzaifa56jbh <hadrish153@gmail.com>
2024-12-08 05:39:18 +0500

    rt

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Services from './components/Services';
 import AdminPanel from './components/Admin';
 import Aboutus from './components/Aboutus';
 import Projects from './components/Projects';
+
 import Project1 from './components/project1';
 import Project2 from './components/project2';
 import Project3 from './components/project3';
